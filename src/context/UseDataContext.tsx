18accbefdbddbb19d887ddb3badfbe72aca1f951
kingahmadr <ahmadnromiz@gmail.com>
2024-08-23 23:20:17 +0700

    refined

--- src/context/UseDataContext.tsx
+++ src/context/UseDataContext.tsx
@@ -1,5 +1,4 @@
 import React, { createContext, useState, useContext, ReactNode } from "react";
-import { useFetchData } from "../modules/UseFetchData";
 
 interface AuthContextType {
   isAuthenticated: boolean;
